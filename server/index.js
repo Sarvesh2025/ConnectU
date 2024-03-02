4c205dc681e7d665d5b1e23a070b0f288c3e59e3
Sarvesh2025 <utsavsarveshpandey@gmail.com>
2024-03-02 22:57:30 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use("/user", userRouter);
 const CONNECTION_URL="mongodb+srv://sarveshprofessional2004:HqHfqelcYvNYwhK4@connectu.okbmttn.mongodb.net/?retryWrites=true&w=majority&appName=connectu"
 const PORT = 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL)
     .then(() => 
      app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
